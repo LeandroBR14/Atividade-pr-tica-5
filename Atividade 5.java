1aa6b4aa221ab221e9c416c5e37b6ae7fce09195
Leandro Scalabrini <scalabrinileandro23@gmail.com>
2023-04-18 22:06:51 -0300

    Atividade 5 finalizada

---
+++ Atividade 5.java
@@ -0,0 +1,25 @@
+/* Nome do Aluno: Leandro Scalabrini
+* RA: 32318945
+* Nome do Programa: 5. Faça um programa que determine o fatorial de um número. Para
+este problema, tem-se como entrada o valor do número do qual
+se deseja calcular o fatorial. O fatorial de 0 é igual a 1. O
+fatorial de um número N (N!) é definido conforme a seguir
+(utilize for):
+i. N! = 1 * 2 * 3 * 4 * ... * (N-1) * N
+* Data de entrega: 30/4/2023
+*/
+
+import java.util.Scanner;
+class Atividade5{
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        System.out.print("Digite um número qualquer: ");
+        int n = teclado.nextInt();
+        int fatorial = 1;
+        int i;
+        for (i = 1; i <= n; i = i + 1) {
+            fatorial = fatorial * i;
+        }
+        System.out.println("O fatorial desse número é: " +fatorial);
+    }
+}
