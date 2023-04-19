777f22fabb95bfff2b9c028ef31382be3dbf4920
Leandro Scalabrini <scalabrinileandro23@gmail.com>
2023-04-18 21:09:38 -0300

    Atividade 2 pronta

---
+++ Atividade 2.java
@@ -0,0 +1,18 @@
+
+
+/* Nome do Aluno: Leandro Scalabrini
+* RA: 32318945
+* Nome do Programa: 2. Faça um programa que imprima todos os números pares de 1 a 100 na ordem inversa
+* Data de entrega: 30/4/2023
+*/
+
+class Atividade2{
+    public static void main(String[] args) {
+        System.out.println("Segue abaixo todos os números pares de 0 a 100 na ordem inversa.");
+        for (int i=100; i >=0; i--){
+            if (i % 2 == 0) {
+                System.out.println(i);
+            }
+        }
+    }    
+}
