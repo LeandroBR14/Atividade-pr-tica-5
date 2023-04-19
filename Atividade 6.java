6a88efb2d35529b60fa739ceaf749f43943cdd2d
Leandro Scalabrini <scalabrinileandro23@gmail.com>
2023-04-19 07:53:23 -0300

    Atividade 6 finalizada

--- Atividade 6.java
+++ Atividade 6.java
@@ -8,15 +8,18 @@ esse volume se torne maior que 1000 centímetros cúbicos.
 * Data de entrega: 30/4/2023
 */
 
+import java.util.Scanner;
 class Atividade6 {
     public static void main(String[] args) {
-        int volumeInicial = 1;
-        int volumeAtual = volumeInicial;
+        Scanner teclado = new Scanner(System.in);
+        System.out.print("Digite o volume do gás em cm³: ");
+        int volumeAtual = teclado.nextInt();
+        teclado.close();
         int tempo = 0;
         while (volumeAtual <= 1000) {
             volumeAtual *= 2;   
             tempo++;
         }
-        System.out.println(" " +volumeAtual +tempo);
+        System.out.printf("É necessário %d segundos para que o volume desse gás chegue em %dcm³", tempo, volumeAtual);
     }
 }
