ff8a83986c94386ef9e36e19e5d2b25af66e4efb
Leandro Scalabrini <scalabrinileandro23@gmail.com>
2023-04-19 07:45:29 -0300

    Att

--- Atividade 5.java
+++ Atividade 5.java
@@ -14,12 +14,13 @@ class Atividade5{
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
         System.out.print("Digite um número qualquer: ");
+        int n = teclado.nextInt();
+        teclado.close();
         int fatorial = 1;
         int i;
         for (i = 1; i <= n; i = i + 1) {
             fatorial = fatorial * i;
         }
         System.out.println("O fatorial desse número é: " +fatorial);
-        teclado.close();
     }
 }
